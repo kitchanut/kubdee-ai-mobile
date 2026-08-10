a96cc0c1e389a73a48472c39ca3003161b3424cf
kitchanut <kitchanut.r@gmail.com>
2026-08-10 22:44:20 +0700

    chore: release mobile v0.9.24

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   android: {
     ...config.android,
     package: 'ai.kubdee.mobile',
-    versionCode: 183,
+    versionCode: 184,
     predictiveBackGestureEnabled: false,
     permissions: [
       'android.permission.INTERNET',
